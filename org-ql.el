706832275725e4d875db0cabb1faa6b40b9f73a2
Adam Porter <adam@alphapapa.net>
2019-07-16 12:36:52 -0500

    Meta: Update version

--- org-ql.el
+++ org-ql.el
@@ -2,7 +2,7 @@
 
 ;; Author: Adam Porter <adam@alphapapa.net>
 ;; Url: http://github.com/alphapapa/org-ql
-;; Version: 0.1
+;; Version: 0.2-pre
 ;; Package-Requires: ((emacs "25.1") (dash "2.13") (org "9.0") (s "1.12.0"))
 ;; Keywords: hypermedia, outlines, Org, agenda
 
