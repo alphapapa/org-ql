edac9c696061aba2a429abb8d61238fa37e28418
Adam Porter <adam@alphapapa.net>
2020-01-18 20:24:28 -0600

    Adjust message, indentation, add changelog entry

--- org-ql-view.el
+++ org-ql-view.el
@@ -300,7 +300,7 @@ update search arguments."
     (or (when (search-forward current-line nil t)
           (beginning-of-line))
         (goto-char old-pos))
-        (message "org-ql: view refreshed")))
+    (message "View refreshed")))
 
 (defun org-ql-view-save ()
   "Save current `org-ql-search' buffer to `org-ql-views'."
