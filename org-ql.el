387594ed368f3ae4fd81c13da6300d978f2f0c34
Ihor Radchenko <yantar92@gmail.com>
2021-04-06 23:02:29 +0800

    Update docstrings

--- org-ql.el
+++ org-ql.el
@@ -1234,7 +1234,7 @@ result form."
 (setf org-ql-defpred-defer t)
 
 (org-ql-defpred org-ql--and (&rest _)
-  "Return non-nil if all the clauses match."
+  "Normalizers and preambles for boolean (and ...) query."
   :normalizers ((`(and)
                  nil)
                 (`(and . ,clauses)
@@ -1263,7 +1263,7 @@ result form."
                        :query `(and ,@queries))))))
 
 (org-ql-defpred org-ql--or (&rest _)
-  "Return non-nil if any of the clauses match."
+  "Normalizers and preambles for boolean (or ...) query."
   :normalizers ((`(or)
                  nil)
                 (`(or . ,clauses)
@@ -1291,8 +1291,8 @@ result form."
                        :case-fold t
                        :query `(or ,@queries))))))
 
-(org-ql-defpred org-ql--when (_ &rest _)
-  "Return values of CLAUSES when CONDITION is non-nil."
+(org-ql-defpred org-ql--when (&rest _)
+  "Normalizers and preambles for (when ...) query."
   :normalizers
   ((`(when ,condition . ,clauses)
     `(when ,(org-ql-normalize-query condition)
@@ -1304,8 +1304,8 @@ result form."
             :case-fold case-fold
             :query `(when ,condition ,@clauses))))))
 
-(org-ql-defpred org-ql--unless (_ &rest _)
-  "Return values of CLAUSES unless CONDITION is non-nil."
+(org-ql-defpred org-ql--unless (&rest _)
+  "Normalizers and preambles for (unless ...) query."
   :normalizers
   ((`(unless ,condition . ,clauses)
     `(unless ,(org-ql-normalize-query condition)
@@ -1318,7 +1318,7 @@ result form."
             :query `(unless ,condition ,@clauses))))))
 
 (org-ql-defpred org-ql--not (_)
-  "Match when CLAUSES don't match."
+  "Normalizers and preambles for (not ...) query."
   :normalizers
   ((`(not . ,clauses)
     `(not ,@(mapcar #'org-ql-normalize-query clauses)))))
